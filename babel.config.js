dfbc65ff0f49fc212a374077d3b023aa33175f4b
yucopowo <yucopowo@gmail.com>
2019-09-02 17:42:26 +0800

    babel add corejs

--- babel.config.js
+++ babel.config.js
@@ -2,42 +2,48 @@ const isProduction = (process.env.NODE_ENV === 'production');
 
 module.exports = {
     "presets": [
-        [
-            "@babel/preset-env",
-            {
-                "modules": false,
 
-                "useBuiltIns": "usage",
-                "corejs": {
-                    "version": 3,
-                    "proposals": true,
-                },
+        ...[].concat(isProduction?[
+            [
+                "@babel/preset-env",
+                {
+                    "modules": false,
 
-                // "useBuiltIns": "usage", //entry
-                // "corejs": 3,
-                // corejs: { version: '3.2', proposals: true },
-                "targets": {
-                    "ie": "9"
+                    "useBuiltIns": "usage",
+                    "corejs": {
+                        "version": 3,
+                        "proposals": true,
+                    },
+
+                    "targets": {
+                        "ie": "9"
+                    }
                 }
-            }
-        ],
+            ]
+        ]:[]),
+
         "@babel/preset-react"
     ],
     "plugins": [
         "@babel/plugin-transform-modules-commonjs",
-        ["@babel/plugin-transform-runtime",
-            {
-                "regenerator": true,
-                "corejs": 3
-            }
-        ],
+
+        ...[].concat(isProduction?[
+            ["@babel/plugin-transform-runtime",
+                {
+                    "regenerator": true,
+                    "corejs": 3
+                }
+            ]
+        ]:[]),
+
         ["@babel/plugin-proposal-decorators", { "legacy": true }],
         ["@babel/plugin-proposal-class-properties", {"loose": true}],
         "@babel/plugin-syntax-dynamic-import",
-        ["component", {
-            "libraryName": "element-ui",
-            "styleLibraryName": "theme-chalk"
-        }
+        [
+            "component", {
+                "libraryName": "element-ui",
+                "styleLibraryName": "theme-chalk"
+            }
         ],
 
         ...[].concat(isProduction?[
